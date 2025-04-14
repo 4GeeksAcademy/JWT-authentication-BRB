0d98e12b6d34666c8976c08ee1d20fb16a476182
Brandon-Ray Brown <77804389+BeCoded126@users.noreply.github.com>
2025-04-14 23:50:28 +0000

    password authentication completed

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,18 +5,45 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-
+from flask_jwt_extended import create_access_token
+from werkzeug.security import generate_password_hash, check_password_hash
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/login', methods=['POST'])
+def login():
+        email_value = request.json.get("email")
+        password = request.json.get("password")
+        find_user = User.query.filter_by(email = email_value).first()
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+        if not check_password_hash(find_user.password, password):
+         return jsonify("Incorrect password!")
 
-    return jsonify(response_body), 200
+        token = create_access_token(identity = email_value)
+
+        return jsonify(token_value = token), 200
+
+
+# look up api endpoint index and why that is used
+
+
+
+@api.route('/signup', methods=['POST'])
+def sign_up():
+        email_value = request.json.get("email")
+        password_value = request.json.get("password")
+        find_user = User.query.filter_by(email = email_value).first()
+        new_user = User(
+                
+            email = email_value,
+            password = generate_password_hash(password_value)
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+
+        return jsonify("user created"), 200
